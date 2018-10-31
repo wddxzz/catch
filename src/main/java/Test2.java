bbf9ef6d0f160356eabcd93037c2e61d2a7ad666
LeiKe <18030212167@163.com>
2018-10-31 20:54:04 +0800

    测试分支

---
+++ src/main/java/Test2.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Xzz on 2018/10/31.
+ */
+
+/**
+ * @author Xzz
+ * @createTime 2018/10/31
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈");
+    }
+}
